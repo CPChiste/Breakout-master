4ea7540e1b4cd15c33658cff3f5e9fd91bcdc04f
CPChiste <cpchiste@gmail.com>
2014-04-13 19:25:18 -0600

    removed menu

--- main.cc
+++ main.cc
@@ -10,30 +10,16 @@
 #include <ncurses.h>
 
 int main(void) {  
-   initscr();
-   noecho();
-   cbreak();
-   keypad(stdscr, TRUE);
-   mvprintw(0,0,"Press q to quit, Spacebar to play.");
-
-   int ch = getch();
-
-   while (ch != ERR && ch != 'Q' && ch != 'q') {
-      if(ch == ' ')
-      {
-	 Display disp;  // create a 1000x1000 window
-	 BreakoutSimulator sim(disp, 100);
-	 BrickGenerator brickGen(disp.getW(), disp.getH());
+   Display disp;  // create a 1000x1000 window
+   BreakoutSimulator sim(disp, 100);
+   BrickGenerator brickGen(disp.getW(), disp.getH());
+   
+   sim = brickGen.generateBricks(sim);
+   
+   sim.addBall(new Ball(Point(400, 400), Vector(200, 200), 8));
+   sim.addPaddle(new Paddle(Point(400, 500), 150, 20));
 	 
-	 sim = brickGen.generateBricks(sim);
-	 
-	 sim.addBall(new Ball(Point(400, 400), Vector(200, 200), 8));
-	 sim.addPaddle(new Paddle(Point(400, 500), 150, 20));
-	 
-	 sim.run();
-
-	 ch = getch();
-      }
-   }
+   sim.run();
+   
    return 0;
 }
