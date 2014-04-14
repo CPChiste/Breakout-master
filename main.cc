04fc30ba7da54579d1b19c0f03685b1531c0418b
CPChiste <cpchiste@gmail.com>
2014-04-13 19:16:39 -0600

    Simple menu added

--- main.cc
+++ main.cc
@@ -7,16 +7,33 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro.h>
+#include <ncurses.h>
 
 int main(void) {  
-   Display disp;  // create a 1000x1000 window
-   BreakoutSimulator sim(disp, 100);
-   BrickGenerator brickGen(disp.getW(), disp.getH());
-   
-   sim = brickGen.generateBricks(sim);
-   
-   sim.addBall(new Ball(Point(400, 400), Vector(200, 200), 8));
-   sim.addPaddle(new Paddle(Point(400, 500), 150, 20));
+   initscr();
+   noecho();
+   cbreak();
+   keypad(stdscr, TRUE);
+   mvprintw(0,0,"Press q to quit, Spacebar to play.");
 
-   sim.run();
+   int ch = getch();
+
+   while (ch != ERR && ch != 'Q' && ch != 'q') {
+      if(ch == ' ')
+      {
+	 Display disp;  // create a 1000x1000 window
+	 BreakoutSimulator sim(disp, 100);
+	 BrickGenerator brickGen(disp.getW(), disp.getH());
+	 
+	 sim = brickGen.generateBricks(sim);
+	 
+	 sim.addBall(new Ball(Point(400, 400), Vector(200, 200), 8));
+	 sim.addPaddle(new Paddle(Point(400, 500), 150, 20));
+	 
+	 sim.run();
+
+	 ch = getch();
+      }
+   }
+   return 0;
 }
